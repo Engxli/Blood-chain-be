a70a144988fd15ba4819c48a5d0c12130ba432b9
Ali Ahmad <turgut@Alis-MBP.net>
2022-06-12 17:45:06 +0300

    Add TimestampMixin

--- shared/models.py
+++ shared/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
