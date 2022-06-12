621b1c1eb5b81711850be4c19cffe368078c5e1e
Ali Ahmad <turgut@Alis-MBP.net>
2022-06-12 15:26:12 +0300

    Remove dead commit message

--- requests/models.py
+++ requests/models.py
@@ -2,26 +2,21 @@ from django.conf import settings
 from django.db import models
 
 
-# from hospitals import models
-
-# Create your models here.
 class Request(models.Model):
-    # blood type choices
     BLOOD_CHOICES = (
-        ("1", "A-"),
-        ("2", "A+"),
-        ("3", "B-"),
-        ("4", "B+"),
-        ("5", "O-"),
-        ("6", "O+"),
-        ("7", "AB-"),
-        ("8", "AB+"),
+        ("A-", "A-"),
+        ("A+", "A+"),
+        ("B-", "B-"),
+        ("B+", "B+"),
+        ("O-", "O-"),
+        ("O+", "O+"),
+        ("AB-", "AB-"),
+        ("AB+", "AB+"),
     )
-    # severity choices
     SEVERITY_CHOICES = (
-        ("1", "LOW"),
-        ("2", "MEDIUM"),
-        ("3", "HIGH"),
+        ("LOW", "LOW"),
+        ("MEDIUM", "MEDIUM"),
+        ("HIGH", "HIGH"),
     )
 
     owner = models.ForeignKey(
@@ -29,10 +24,9 @@ class Request(models.Model):
         on_delete=models.CASCADE,
         related_name="request_owner",
     )
-    blood_type = models.CharField(max_length=1, choices=BLOOD_CHOICES)
-    severity = models.CharField(max_length=1, choices=SEVERITY_CHOICES)
-    quantity = models.IntegerField()
-    # hospital = models.ForeignKey(models.Hospital, on_delete=models.CASCADE,related_name="request")
+    blood_type = models.CharField(max_length=3, choices=BLOOD_CHOICES)
+    severity = models.CharField(max_length=6, choices=SEVERITY_CHOICES)
+    quantity = models.PositiveIntegerField()
     details = models.TextField()
     donors = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
