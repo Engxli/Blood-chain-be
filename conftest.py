2f1f482d8723c3c63efa976e5157c7f0775c3f21
Ali Ahmad <turgut@Alis-MBP.net>
2022-06-12 22:58:12 +0300

    Add conftest.py file

---
+++ conftest.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+import pytest
+from django.test import Client
+from graphene_django.utils.testing import graphql_query
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def client_query(client: Client) -> Any:
+    def func(*args: Any, **kwargs: Any) -> Any:
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
