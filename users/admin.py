5b56cafe996948e3cd2be3a48b5a1d8b234f127a
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-16 09:17:20 -0500

    Add UserProfile model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import CustomUser
+from .models import CustomUser, UserProfile
 
 
-admin.site.register(CustomUser)
+admin.site.register([CustomUser, UserProfile])
--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="profile"
+    )
+    crypto_wallet = models.CharField(null=True, blank=True, max_length=35)
+    phone = models.CharField(max_length=8, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.email}"
