763a0eb8d8ec788081afc29a5a887f38d7d0d0a7
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-18 10:55:39 -0500

    Add UserQuery with profile

--- users/queries.py
+++ users/queries.py
@@ -1,6 +1,16 @@
 import graphene
 from graphql_auth.schema import MeQuery
 
+from users.types import UserProfileType
+
+from .models import UserProfile
+
 
 class UserQuery(MeQuery, graphene.ObjectType):
-    pass
+    profile = graphene.Field(UserProfileType)
+
+    def resolve_profile(root, info: graphene.ResolveInfo) -> UserProfile:
+        profile = UserProfile.objects.get(user_id=info.context.user.id)
+        if profile:
+            return profile
+        return UserProfile.objects.create(user=info.context.user)
