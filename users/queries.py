3b048009b1a91db56fef6d0bb3a82cae7a447db0
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-18 11:00:40 -0500

    Change import style

--- users/queries.py
+++ users/queries.py
@@ -1,4 +1,4 @@
-import graphene
+from graphene import Field, ObjectType, ResolveInfo
 from graphql_auth.schema import MeQuery
 
 from users.types import UserProfileType
@@ -6,10 +6,10 @@ from users.types import UserProfileType
 from .models import UserProfile
 
 
-class UserQuery(MeQuery, graphene.ObjectType):
-    profile = graphene.Field(UserProfileType)
+class UserQuery(MeQuery, ObjectType):
+    profile = Field(UserProfileType)
 
-    def resolve_profile(root, info: graphene.ResolveInfo) -> UserProfile:
+    def resolve_profile(root, info: ResolveInfo) -> UserProfile:
         profile = UserProfile.objects.get(user_id=info.context.user.id)
         if profile:
             return profile
