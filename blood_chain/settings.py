efa9392105d58d94fbe33145d21b0d9b4f9d0260
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-06-23 08:15:03 +0300

    Add deployment setup

--- blood_chain/settings.py
+++ blood_chain/settings.py
@@ -167,3 +167,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import django_heroku
+
+
+django_heroku.settings(locals())
