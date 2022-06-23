acc0faae88dbc672957dc47b07cc4f07189ae972
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-06-23 08:04:01 +0300

    Reference correct user model

--- users/signals.py
+++ users/signals.py
@@ -1,18 +1,26 @@
 # pylint: disable=unused-argument
-from typing import Any, Type
+from typing import TYPE_CHECKING, Any, Type
 
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from users.models import CustomUser, UserProfile
+from users.models import UserProfile
 
 
-@receiver(post_save, sender=CustomUser)
+if TYPE_CHECKING:
+    from users.models import CustomUser
+
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
 def create_profile(
-    sender: Type[CustomUser],
-    instance: CustomUser,
+    sender: Type["CustomUser"],
+    instance: "CustomUser",
     created: bool,
-    **kwargs: Any
+    **kwargs: Any,
 ) -> None:
     if created:
         UserProfile.objects.create(user=instance)
