bb8d500eb0a48a0baa74a2d2d7920b08856d43d0
Ali Ahmad <turgut@Alis-MBP.net>
2022-06-24 16:38:40 +0300

    Add blood_type filed to profile model

--- users/models.py
+++ users/models.py
@@ -8,6 +8,16 @@ class CustomUser(AbstractUser):
 
 
 class UserProfile(models.Model):
+    class BloodType(models.TextChoices):
+        Amin = "A-"
+        Apos = "A+"
+        Omin = "O-"
+        Opos = "O+"
+        Bmin = "B-"
+        Bpos = "B+"
+        ABmin = "AB-"
+        ABpos = "AB+"
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -15,6 +25,9 @@ class UserProfile(models.Model):
     )
     crypto_wallet = models.CharField(max_length=35, default="", blank=True)
     phone = models.CharField(max_length=8, default="", blank=True)
+    blood_type = models.CharField(
+        max_length=3, choices=BloodType.choices, blank=True
+    )
 
     def __str__(self) -> str:
         return self.user.email
