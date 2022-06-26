8438f1d76624d8467737d9a06523358368fbfe2b
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-26 08:17:08 -0600

    Add pending donations query

--- donations/queries.py
+++ donations/queries.py
@@ -1,8 +1,22 @@
+from typing import Any, Iterable
+
 import graphene
 import graphene_django
+from graphql import GraphQLError
 
-from requests import types
+from donations import models, types
 
 
 class DonationQuery(graphene.ObjectType):
     donations = graphene_django.DjangoListField(types.DonationType)
+    pending_donations = graphene_django.DjangoListField(types.DonationType)
+
+    def resolve_pending_donations(
+        root, info: graphene.ResolveInfo, **kwargs: Any
+    ) -> Iterable[models.Donation]:
+        try:
+            return models.Donation.objects.filter(
+                status="PENDING", donor__user_id=info.context.user.id
+            )
+        except models.Donation.DoesNotExist as exc:
+            raise GraphQLError(str(exc))
