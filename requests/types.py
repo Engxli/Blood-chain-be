475271866a1e782540b96781e45c8f4cd6f1ee4e
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-26 07:14:55 -0600

    Add donations to request type

--- requests/types.py
+++ requests/types.py
@@ -1,6 +1,7 @@
 import graphene
 import graphene_django
 
+from donations.types import DonationType
 from requests import models
 from shared.utils import get_graphene_enum
 from users.types import UserProfileType
@@ -12,6 +13,7 @@ BloodTypeEnum = get_graphene_enum(models.Request.BloodType)
 class RequestType(graphene_django.DjangoObjectType):
     owner = graphene.Field(UserProfileType)
     blood_type = graphene.Field(BloodTypeEnum)
+    donations = graphene.Field(DonationType)
 
     class Meta:
         model = models.Request
