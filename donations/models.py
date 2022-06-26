774ec6ad3ddd72812408d11b63a557c1fb0891a2
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-26 07:13:58 -0600

    Add Donations model

--- donations/models.py
+++ donations/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
+from requests.models import Request
+from shared.models import TimestampMixin
+from users.models import UserProfile
 
-class Donation(models.Model):
-    ...
+
+class Donation(TimestampMixin, models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING"
+        COMPLETE = "COMPLETE"
+        CANCELED = "CANCELED"
+
+    status = models.CharField(max_length=8, choices=Status.choices)
+    request = models.ForeignKey(
+        Request, on_delete=models.CASCADE, related_name="donations"
+    )
+    donor = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="donations"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.donor} Donation for {self.request.owner} request"
