3386988dc21960bbf832de89541b400b808dd2e0
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-06-26 08:16:26 -0600

    Modify and resolve donations

--- requests/types.py
+++ requests/types.py
@@ -1,6 +1,9 @@
+from typing import Iterable
+
 import graphene
 import graphene_django
 
+from donations.models import Donation
 from donations.types import DonationType
 from requests import models
 from shared.utils import get_graphene_enum
@@ -13,7 +16,12 @@ BloodTypeEnum = get_graphene_enum(models.Request.BloodType)
 class RequestType(graphene_django.DjangoObjectType):
     owner = graphene.Field(UserProfileType)
     blood_type = graphene.Field(BloodTypeEnum)
-    donations = graphene.Field(DonationType)
+    donations = graphene.List(DonationType)
+
+    def resolve_donations(
+        self, info: graphene.ResolveInfo
+    ) -> Iterable[Donation]:
+        return Donation.objects.filter(request_id=self.id)
 
     class Meta:
         model = models.Request
