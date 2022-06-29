c8193a68fcd1ce1a2f6b97f3cb0b2d1a087187be
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-06-29 12:01:16 +0300

    Add type hints

--- users/models.py
+++ users/models.py
@@ -1,8 +1,14 @@
+from typing import Optional, Union
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.expressions import Combinable
 
 from shared.enums import BloodType as _BloodType
+
+
+_User = models.OneToOneField[Union["CustomUser", Combinable], "CustomUser"]
 
 
 class CustomUser(AbstractUser):
@@ -12,14 +18,14 @@ class CustomUser(AbstractUser):
 class UserProfile(models.Model):
     BloodType = _BloodType
 
-    user = models.OneToOneField(
+    user: _User = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="profile",
     )
     crypto_wallet = models.CharField(max_length=35, default="", blank=True)
     phone = models.CharField(max_length=8, default="", blank=True)
-    blood_type = models.CharField(
+    blood_type: models.CharField[_BloodType, Optional[str]] = models.CharField(
         max_length=3, choices=BloodType.choices, null=True
     )
 
