5cd04bf404a72a778299142cbdf3afdeb18d27c0
Ali Ahmad <turgut@Alis-MBP.net>
2022-07-05 00:27:19 +0300

    Fix bug when creating user from admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from users.models import CustomUser, UserProfile
+from users import models
 
 
-admin.site.register([CustomUser, UserProfile])
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin, admin.ModelAdmin[models.CustomUser]):
+    pass
+
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin[models.UserProfile]):
+    pass
