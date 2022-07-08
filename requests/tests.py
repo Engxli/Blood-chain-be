247dd2d4f04b2fc8d58ae82dec67e2d3618ff2f6
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-07-08 17:57:57 +0300

    Revert test changes

--- requests/tests.py
+++ requests/tests.py
@@ -1,175 +1,146 @@
-# import json
-# from typing import Any
+import json
 
-# import pytest
-# from django.contrib.auth import get_user_model
+import pytest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
-# from conftest import ClientQuery
-# from donations.models import Donation
-# from requests.models import Request
-# from users.models import UserProfile
-
-
-# # @pytest.fixture
-# # @pytest.mark.django_db
-# # def request_() -> Request:
-# #     User = get_user_model()
-# #     user1 = User.objects.create(username="user1", password="ILoveDjango!")
-# #     user2 = User.objects.create(username="user2", password="ILoveDjango!")
-# #     request = Request.objects.create(
-# #         blood_type=Request.BloodType.Omin,
-# #         severity=Request.Severity.HIGH,
-# #         quantity=500,
-# #         details="No details yet!",
-# #     )
-# #     request.donors.add(user1, user2)
-# #     return request
+from conftest import ClientQuery
+from requests.models import Request
+from users.models import CustomUser
 
 
-# # @pytest.mark.django_db
-# # def test_request_query(client_query: ClientQuery, request_: Request) -> None:
-# #     blood_type = {
-# #         "A+": "A__1",
-# #         "A-": "A_",
-# #         "B+": "B__3",
-# #         "B-": "B_",
-# #         "O+": "O__5",
-# #         "O-": "O_",
-# #         "AB+": "AB__7",
-# #         "AB-": "AB_",
-# #     }
-# #     response = client_query(
-# #         f"""
-# #         query{{
-# #             request(id: {request_.id}) {{
-# #                 id
-# #                 createdAt
-# #                 modifiedAt
-# #                 owner {{
-# #                 id
-# #                 username
-# #                 }}
-# #                 bloodType
-# #                 severity
-# #                 quantity
-# #                 details
-# #                 donors {{
-# #                 id
-# #                 username
-# #                 }}
-# #             }}
-# #         }}
-# #         """
-# #     )
-# #     content = json.loads(response.content)
-# #     assert "errors" not in content
-# #     data = content["data"]["request"]
-# #     assert data["owner"]["id"] == str(request_.owner.id)
-# #     assert data["owner"]["username"] in request_.owner.username
-# #     assert data["bloodType"] in blood_type[request_.blood_type]
-# #     assert data["severity"] in request_.severity
-# #     assert data["quantity"] == request_.quantity
-# #     assert data["details"] in request_.details
-# #     for donor in data["donors"]:
-# #         assert donor["id"] in [
-# #             str(donors.id) for donors in request_.donors.all().iterator()
-# #         ]
-# #         assert donor["username"] in [
-# #             donors.username for donors in request_.donors.all().iterator()
-# #         ]
+User = get_user_model()
 
 
-# @pytest.fixture
-# @pytest.mark.django_db
-# def requests() -> list[Request]:
-#     User = get_user_model()
-#     user1 = User.objects.create(username="admin1", password="ILoveDjango!")
-#     user1_profile = UserProfile.objects.get(user=user1)
-#     user2 = User.objects.create(username="admin2", password="ILoveDjango!")
-#     user2_profile = UserProfile.objects.get(user=user2)
-#     user3 = User.objects.create(username="admin3", password="ILoveDjango!")
-#     user3_profile = UserProfile.objects.get(user=user3)
+def create_user(username: str, password: str = "ILoveDjango!") -> CustomUser:
+    return User.objects.create(
+        username=username, password=make_password(password)
+    )
 
-#     request1 = Request.objects.create(
-#         owner=user1_profile,
-#         blood_type=Request.BloodType.Omin,
-#         severity=Request.Severity.HIGH,
-#         quantity=500,
-#         details="No details yet!",
-#     )
 
-#     Donation.objects.create(
-#         status=Donation.Status.COMPLETE, request=request1, donor=user3_profile
-#     )
+@pytest.fixture
+@pytest.mark.django_db
+def request_() -> Request:
+    user1 = create_user(username="user1")
+    request = Request.objects.create(
+        owner=user1.profile,
+        blood_type=Request.BloodType.Omin,
+        severity=Request.Severity.HIGH,
+        quantity=500,
+        details="No details yet!",
+    )
+    return request
 
-#     request2 = Request.objects.create(
-#         owner=user2_profile,
-#         blood_type=Request.BloodType.Omin,
-#         severity=Request.Severity.HIGH,
-#         quantity=500,
-#         details="No details yet!",
-#     )
 
-#     Donation.objects.create(
-#         status=Donation.Status.COMPLETE, request=request2, donor=user3_profile
-#     )
+@pytest.mark.django_db
+def test_request_query(client_query: ClientQuery, request_: Request) -> None:
+    response = client_query(
+        f"""
+        query {{
+            request(id: {request_.id}) {{
+                id
+                owner {{
+                    id
+                }}
+                bloodType
+                severity
+                quantity
+                details
+            }}
+        }}
+        """
+    )
 
-#     return [request1, request2]
+    content = json.loads(response.content)
+    assert "errors" not in content
 
-# @pytest.mark.django_db
-# def test_requests_query(
-#     client_query: ClientQuery, requests: list[Request], capsys: Any
-# ) -> None:
-#     blood_type = {
-#         "A+": "A__1",
-#         "A-": "A_",
-#         "B+": "B__3",
-#         "B-": "B_",
-#         "O+": "O__5",
-#         "O-": "O_",
-#         "AB+": "AB__7",
-#         "AB-": "AB_",
-#     }
-#     response = client_query(
-#         """
-#         query{
-#             requests {
-#                         id
-#                         createdAt
-#                         modifiedAt
-#                         owner {
-#                         id
-#                         user{
-#                             username
-#                         }
-#                         }
-#                         bloodType
-#                         severity
-#                         quantity
-#                         details
-#                 }
-#             }
-#         """
-#     )
-#     content = json.loads(response.content)
+    data = content["data"]["request"]
+    assert data["owner"]["id"] == str(request_.owner.id)
+    assert data["bloodType"] == Request.BloodType(request_.blood_type).name
+    assert data["severity"] in request_.severity
+    assert data["quantity"] == request_.quantity
+    assert data["details"] in request_.details
 
-#     with capsys.disabled():
-#         print(content)
 
-#     # assert "errors" not in content
-#     # data = content["data"]["requests"]
-#     # for request, data_request in zip(requests, data):
-#     #     assert data_request["owner"]["id"] == str(request.owner.id)
-#     #     assert data_request["owner"]["id"] == str(request.owner.id)
-#     #     assert data_request["owner"]["username"] in request.owner.username
-#     #     assert data_request["bloodType"] in blood_type[request.blood_type]
-#     #     assert data_request["severity"] in request.severity
-#     #     assert data_request["quantity"] == request.quantity
-#     #     assert data_request["details"] in request.details
-#     #     for donor in data_request["donors"]:
-#     #         assert donor["id"] in [
-#     #             str(donors.id) for donors in request.donors.all().iterator()
-#     #         ]
-#     #         assert donor["username"] in [
-#     #             donors.username for donors in request.donors.all().iterator()
-#     #         ]
+@pytest.fixture
+@pytest.mark.django_db
+def requests() -> list[Request]:
+    user1 = create_user(username="admin1")
+    user2 = create_user(username="admin2")
+
+    request1 = Request.objects.create(
+        owner=user1.profile,
+        blood_type=Request.BloodType.Omin,
+        severity=Request.Severity.HIGH,
+        quantity=500,
+        details="No details yet!",
+    )
+    request2 = Request.objects.create(
+        owner=user2.profile,
+        blood_type=Request.BloodType.Omin,
+        severity=Request.Severity.HIGH,
+        quantity=500,
+        details="No details yet!",
+    )
+
+    return [request1, request2]
+
+
+@pytest.mark.django_db
+def test_requests_query_eligible_false(
+    client_query: ClientQuery, requests: list[Request]
+) -> None:
+    response = client_query(
+        """
+        query {
+            requests(onlyEligible:false) {
+                id
+                owner {
+                    id
+                    bloodType
+                }
+                bloodType
+                severity
+                quantity
+                details
+            }
+        }
+        """
+    )
+
+    content = json.loads(response.content)
+    assert "errors" not in content
+
+    all_data = content["data"]["requests"]
+    for request, data in zip(requests, all_data):
+        assert data["owner"]["id"] == str(request.owner.id)
+        assert data["bloodType"] == Request.BloodType(request.blood_type).name
+        assert data["severity"] in request.severity
+        assert data["quantity"] == request.quantity
+        assert data["details"] in request.details
+
+
+@pytest.mark.django_db
+def test_requests_query_eligible_true_not_logged_in(
+    client_query: ClientQuery,
+) -> None:
+    response = client_query(
+        """
+        query {
+            requests(onlyEligible:true) {
+                id
+                owner {
+                    id
+                    bloodType
+                }
+                bloodType
+                severity
+                quantity
+                details
+            }
+        }
+        """
+    )
+
+    content = json.loads(response.content)
+    assert "errors" in content
