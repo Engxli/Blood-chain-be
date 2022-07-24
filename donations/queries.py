03a0f43b7ad3d2954589e0a1e2e510786f4f2f26
abdulaziz <abdulaziz.alsinafi@gmail.com>
2022-07-24 05:57:38 -0400

    Add request based donation query

--- donations/queries.py
+++ donations/queries.py
@@ -8,11 +8,13 @@ from donations import models, types
 
 
 class DonationQuery(graphene.ObjectType):
-    donations = graphene_django.DjangoListField(
+    userDonations = graphene_django.DjangoListField(
         types.DonationType, pending=graphene.Boolean()
     )
-
-    def resolve_donations(
+    requestDonations = graphene_django.DjangoListField(
+        types.DonationType, request_id = graphene.String()
+    )
+    def resolve_userDonations(
         root, info: graphene.ResolveInfo, **kwargs: Any
     ) -> QuerySet[models.Donation]:
 
@@ -25,3 +27,6 @@ class DonationQuery(graphene.ObjectType):
         return models.Donation.objects.filter(
             donor__user_id=info.context.user.id
         )
+        
+    def resolve_requestDonations(root, info: graphene.ResolveInfo,request_id: int) -> QuerySet[models.Donation]:
+        return models.Donation.objects.filter(request__id = request_id)
