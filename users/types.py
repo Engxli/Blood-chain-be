acd7a2d1380971108a8d0191eec782642ef1a3ad
abdulaziz <abdulaziz.alsinafi@gmail.com>
2022-07-26 12:06:07 -0400

    Add can_donate field to profile query

--- users/types.py
+++ users/types.py
@@ -1,6 +1,8 @@
+import graphene
 import graphene_django
 from django.contrib.auth import get_user_model
 
+from donations import models
 from users.models import UserProfile
 
 
@@ -13,5 +15,10 @@ class UserType(graphene_django.DjangoObjectType):
 
 
 class UserProfileType(graphene_django.DjangoObjectType):
+    can_donate = graphene.Boolean()
     class Meta:
         model = UserProfile
+        
+    def resolve_can_donate (self, info: graphene.ResolveInfo) -> bool:
+        profile = models.UserProfile.objects.get(pk = self.id)
+        return profile.can_donate
