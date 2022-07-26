316e3566e2f2b42f4e47473277d236433c51d5a9
F Almumen <fatemah.alm@hotmail.com>
2022-07-26 20:19:14 +0300

    Remove django-admin-interface library

--- blood_chain/settings.py
+++ blood_chain/settings.py
@@ -41,8 +41,6 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 INSTALLED_APPS = [
-    "admin_interface",
-    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
